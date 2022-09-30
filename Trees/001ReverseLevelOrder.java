9e171bd9704a54abfc6eabe7c3de70c629991dbb
Sourabh Kumar <55038876+sourabhkumar45@users.noreply.github.com>
2022-09-30 13:36:02 +0530

    started tree

---
+++ Trees/001ReverseLevelOrder.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> smallAns = new ArrayList<>();
+        reverseLevel(root, ans, smallAns, 0);
+        Collections.reverse(ans);
+        return ans;
+    }
+    public void reverseLevel(TreeNode root, List<List<Integer>> ans, List<Integer> smallAns, int lvl) {
+        if(root==null) return;
+        
+        if(lvl == ans.size()) {
+            ans.add(new ArrayList<>());
+        }
+      
+        reverseLevel(root.left, ans, smallAns, lvl+1);
+        reverseLevel(root.right, ans, smallAns, lvl+1);
+        ans.get(lvl).add(root.val);
+    }
+}
